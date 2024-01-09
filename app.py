3250c747016ea6220a25c98a0d0fcfcdcfd7e5ac
onekiguwolf <onekiguwolf@gmail.com>
2024-01-09 15:31:55 +0800

    oops forgot to push main

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from interpolation_search import interpolation_search, interpolation_search_wrap
 from jump_search import jump_search, jump_search_wrapper
 from linear_search import linear_search, linear_search_wrapper
 from ternary_search import ternary_search, ternary_search_wrapper
+from postfix import infix_to_postfix
 
 app = Flask(__name__, static_url_path='/static')
 
@@ -25,9 +26,20 @@ def Memberpage():
 def ResultsPage():
     return render_template('ResultsPage.html')
 
-@app.route('/templates/Postfix.html')
+@app.route('/templates/Postfix.html', methods=["GET", "POST"])
 def Postfix():
-    return render_template('Postfix.html')
+    # Request Infix Expression
+    if request.method == "POST":
+        infix_str = request.form.get("infix")
+
+        try:
+            float(infix_str)
+            return render_template("Postfix.html", error="Invalid input. Ensure the operators are only `*`, `(`, `)`, `+`, `-`, or `/`, and free from numbers or whitespaces.")
+        except:
+            result = infix_to_postfix(infix_str)
+            return render_template("Postfix.html", result=result, infix_str=infix_str)
+    else:
+        return render_template('Postfix.html', result=None)
 
 @app.route("/templates/searchalgo-interface.html", methods=["GET", "POST"])
 def dir():
