d9f8e766a30cea34f471ba1f48021edb9069337a
onekiguwolf <onekiguwolf@gmail.com>
2024-02-06 01:50:30 +0800

    still updating uggh

--- graph.py
+++ graph.py
@@ -15,6 +15,19 @@ def build_mrt_lrt_graph():
     # Create a graph representing MRT and LRT lines in the Philippines
     mrt_lrt_graph = Graph()
     
+    mrt_lrt_graph.add_vertex('North Avenue')
+    mrt_lrt_graph.add_vertex('Quezon Avenue')
+    mrt_lrt_graph.add_vertex('Kamuning')
+    mrt_lrt_graph.add_vertex('MRT Cubao')
+    mrt_lrt_graph.add_vertex('MRT Santolan')
+    mrt_lrt_graph.add_vertex('Ortigas')
+    mrt_lrt_graph.add_vertex('Shaw Boulevard')
+    mrt_lrt_graph.add_vertex('Boni')
+    mrt_lrt_graph.add_vertex('Guadalupe')
+    mrt_lrt_graph.add_vertex('Buendia')
+    mrt_lrt_graph.add_vertex('Ayala')
+    mrt_lrt_graph.add_vertex('Magallanes')
+    mrt_lrt_graph.add_vertex('Taft Avenue')
     mrt_lrt_graph.add_vertex('Baclaran')
     mrt_lrt_graph.add_vertex('EDSA')
     mrt_lrt_graph.add_vertex('Libertad')
@@ -28,13 +41,6 @@ def build_mrt_lrt_graph():
     mrt_lrt_graph.add_vertex('Doroteo Jose')
     mrt_lrt_graph.add_vertex('Bambang')
     mrt_lrt_graph.add_vertex('Tayuman')
-    mrt_lrt_graph.add_vertex('Blumentritt')
-    mrt_lrt_graph.add_vertex('Abad Santos')
-    mrt_lrt_graph.add_vertex('R. Papa')
-    mrt_lrt_graph.add_vertex('5th Avenue')
-    mrt_lrt_graph.add_vertex('Monumento')
-    mrt_lrt_graph.add_vertex('Balintawak')
-    mrt_lrt_graph.add_vertex('Fernando Poe Jr.')
     mrt_lrt_graph.add_vertex('Recto')
     mrt_lrt_graph.add_vertex('Legarda')
     mrt_lrt_graph.add_vertex('Pureza')
@@ -42,26 +48,30 @@ def build_mrt_lrt_graph():
     mrt_lrt_graph.add_vertex('J. Ruiz')
     mrt_lrt_graph.add_vertex('Gilmore')
     mrt_lrt_graph.add_vertex('Betty Go-Belmonte')
-    mrt_lrt_graph.add_vertex('Araneta-Center Cubao')
+    mrt_lrt_graph.add_vertex('Araneta Center-Cubao')
     mrt_lrt_graph.add_vertex('Anonas')
     mrt_lrt_graph.add_vertex('Katipunan')
-    mrt_lrt_graph.add_vertex('Santolan')
+    mrt_lrt_graph.add_vertex('LRT Santolan')
     mrt_lrt_graph.add_vertex('Marikina')
     mrt_lrt_graph.add_vertex('Antipolo')
-    mrt_lrt_graph.add_vertex('North Avenue')
-    mrt_lrt_graph.add_vertex('Quezon Avenue')
-    mrt_lrt_graph.add_vertex('GMA-Kamuning')
-    mrt_lrt_graph.add_vertex('MRT Cubao')
-    mrt_lrt_graph.add_vertex('Santolan Annapolis')
-    mrt_lrt_graph.add_vertex('Ortigas')
-    mrt_lrt_graph.add_vertex('Shaw Boulevard')
-    mrt_lrt_graph.add_vertex('Boni')
-    mrt_lrt_graph.add_vertex('Guadalupe')
-    mrt_lrt_graph.add_vertex('Buendia')
-    mrt_lrt_graph.add_vertex('Ayala')
-    mrt_lrt_graph.add_vertex('Magallanes')
-    mrt_lrt_graph.add_vertex('Taft Avenue')
 
+  # MRT Line 3
+    mrt_lrt_graph.add_edge('North Avenue', 'Quezon Avenue')
+    mrt_lrt_graph.add_edge('Quezon Avenue', 'Kamuning')
+    mrt_lrt_graph.add_edge('Kamuning', 'MRT Cubao')
+    mrt_lrt_graph.add_edge('MRT Cubao', 'MRT Santolan')
+    mrt_lrt_graph.add_edge('MRT Santolan', 'Ortigas')
+    mrt_lrt_graph.add_edge('Ortigas', 'Shaw Boulevard')
+    mrt_lrt_graph.add_edge('Shaw Boulevard', 'Boni')
+    mrt_lrt_graph.add_edge('Boni', 'Guadalupe')
+    mrt_lrt_graph.add_edge('Guadalupe', 'Buendia')
+    mrt_lrt_graph.add_edge('Buendia', 'Ayala')
+    mrt_lrt_graph.add_edge('Ayala', 'Magallanes')
+    mrt_lrt_graph.add_edge('Magallanes', 'Taft Avenue')
+
+    # MRT to LRT Line 2 (interchange at Araneta Center-Cubao)
+    mrt_lrt_graph.add_edge('MRT Cubao', 'Araneta Center-Cubao')
+    mrt_lrt_graph.add_edge('Araneta Center-Cubao', 'MRT Cubao')
 
     # LRT Line 1
     mrt_lrt_graph.add_edge('Baclaran', 'EDSA')
@@ -76,20 +86,10 @@ def build_mrt_lrt_graph():
     mrt_lrt_graph.add_edge('Carriedo', 'Doroteo Jose')
     mrt_lrt_graph.add_edge('Doroteo Jose', 'Bambang')
     mrt_lrt_graph.add_edge('Bambang', 'Tayuman')
-    mrt_lrt_graph.add_edge('Tayuman', 'Blumentritt')
-    mrt_lrt_graph.add_edge('Blumentritt', 'Abad Santos')
-    mrt_lrt_graph.add_edge('Abad Santos', 'R. Papa')
-    mrt_lrt_graph.add_edge('R. Papa', '5th Avenue')
-    mrt_lrt_graph.add_edge('5th Avenue', 'Monumento')
-    mrt_lrt_graph.add_edge('Monumento', 'Balintawak')
-    mrt_lrt_graph.add_edge('Balintawak', 'Fernando Poe Jr.')
-    
-    # LRT Line 1 to MRT Line 3
-    mrt_lrt_graph.add_edge('EDSA', 'Taft Avenue')
-    mrt_lrt_graph.add_edge('Fernando Poe Jr.', 'North Avenue')
-    
-    # LRT Line 1 to LRT Line 2
+
+    # LRT Line 1 to LRT Line 2 (interchange at Doroteo Jose)
     mrt_lrt_graph.add_edge('Doroteo Jose', 'Recto')
+    mrt_lrt_graph.add_edge('Recto', 'Doroteo Jose')
 
     # LRT Line 2
     mrt_lrt_graph.add_edge('Recto', 'Legarda')
@@ -98,32 +98,18 @@ def build_mrt_lrt_graph():
     mrt_lrt_graph.add_edge('V. Mapa', 'J. Ruiz')
     mrt_lrt_graph.add_edge('J. Ruiz', 'Gilmore')
     mrt_lrt_graph.add_edge('Gilmore', 'Betty Go-Belmonte')
-    mrt_lrt_graph.add_edge('Betty Go-Belmonte', 'Araneta-Center Cubao')
-    mrt_lrt_graph.add_edge('Araneta-Center Cubao', 'Anonas')
+    mrt_lrt_graph.add_edge('Betty Go-Belmonte', 'Araneta Center-Cubao')
+    mrt_lrt_graph.add_edge('Araneta Center-Cubao', 'Anonas')
     mrt_lrt_graph.add_edge('Anonas', 'Katipunan')
     mrt_lrt_graph.add_edge('Katipunan', 'LRT Santolan')
     mrt_lrt_graph.add_edge('LRT Santolan', 'Marikina')
     mrt_lrt_graph.add_edge('Marikina', 'Antipolo')
     
-        # MRT Line 3
-    mrt_lrt_graph.add_edge('North Avenue', 'Quezon Avenue')
-    mrt_lrt_graph.add_edge('Quezon Avenue', 'GMA-Kamuning')
-    mrt_lrt_graph.add_edge('GMA-Kamuning', 'MRT Cubao')
-    mrt_lrt_graph.add_edge('MRT Cubao', 'Santolan Annapolis')
-    mrt_lrt_graph.add_edge('Santolan Annapolis', 'Ortigas')
-    mrt_lrt_graph.add_edge('Ortigas', 'Shaw Boulevard') 
-    mrt_lrt_graph.add_edge('Shaw Boulevard', 'Boni')
-    mrt_lrt_graph.add_edge('Boni', 'Guadalupe')
-    mrt_lrt_graph.add_edge('Guadalupe', 'Buendia')
-    mrt_lrt_graph.add_edge('Buendia', 'Ayala')
-    mrt_lrt_graph.add_edge('Ayala', 'Magallanes')
-    mrt_lrt_graph.add_edge('Magallanes', 'Taft Avenue')
-    
-    # MRT to LRT Line 2
-    mrt_lrt_graph.add_edge('MRT Cubao', 'Araneta-Center Cubao')
-
-
+    #LRT Line 1 and MRT Line 3
+    mrt_lrt_graph.add_edge('Taft Avenue', 'EDSA')
+    mrt_lrt_graph.add_edge('EDSA', 'Taft Avenue')
     return mrt_lrt_graph
+
 
 def find_shortest_path(graph, start, end):
     # Perform a Breadth-First Search (BFS) to find the shortest path
@@ -134,6 +120,8 @@ def find_shortest_path(graph, start, end):
         current_station, path = queue.pop(0)
         path = path + [current_station]
 
+        print(f"Visiting: {current_station}")
+
         if current_station == end:
             return path
 
@@ -143,17 +131,36 @@ def find_shortest_path(graph, start, end):
             if neighbor not in visited:
                 queue.append((neighbor, path))
 
+        # Special handling for interchange stations
+        if current_station == 'Araneta Center-Cubao':
+            for neighbor in graph.graph[current_station]:
+                if neighbor not in path and neighbor not in visited:
+                    queue.append((neighbor, path + [neighbor]))
+        elif current_station == 'Doroteo Jose':
+            for neighbor in graph.graph[current_station]:
+                if neighbor not in path and neighbor not in visited:
+                    queue.append((neighbor, path + [neighbor]))
+
     return None  # If no path is found
+
+
+# Add methods to the Graph class to get forward and reverse neighbors
+def get_forward_neighbor(self, station, path):
+    for neighbor in self.graph[station]:
+        if neighbor not in path:
+            return neighbor
+    return None
+
+def get_reverse_neighbor(self, station, path):
+    for neighbor in self.graph[station]:
+        if neighbor not in path:
+            return neighbor
+    return None
+
+# Add the new methods to the Graph class
+Graph.get_forward_neighbor = get_forward_neighbor
+Graph.get_reverse_neighbor = get_reverse_neighbor
 
 # Build the MRT and LRT graph
 mrt_lrt_graph = build_mrt_lrt_graph()
 
-# Find the shortest path between two stations
-# start_station = input("Enter start station:\n")
-# end_station = input("Enter end station:\n ")
-# shortest_path = find_shortest_path(mrt_lrt_graph, start_station, end_station)
-
-# if shortest_path:
-#     print(f"Shortest path from {start_station} to {end_station}: {shortest_path}")
-# else:
-#     print(f"No path found from {start_station} to {end_station}")
